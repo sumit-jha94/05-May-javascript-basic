05336571b38b40de1ca3d99f61a10de8598968d4
sumit-jha94 <sumitshine14@gmail.com>
2023-06-05 18:51:53 +0530

    Data Types

---
+++ data-types.js
@@ -0,0 +1,27 @@
+// // Primitive Data Types
+
+// let a = null;
+// let b = 345;
+// let c = true;
+// let d = BigInt ("456")
+// let e = "Sumit";
+// let f = Symbol("I am sumit")
+// let g
+// let h = undefined
+
+
+// console.log(a,b,c,d,e,f,g,h)
+
+// Object in JS
+
+const list = {
+    "Sumit": true,
+    "Love": false,
+    "Deepu": 67,
+    "Ramesh": undefined
+}
+
+console.log(list);
+console.log(list["Sumit"]);
+console.log(typeof list);
+console.log(typeof list["Sumit"])
