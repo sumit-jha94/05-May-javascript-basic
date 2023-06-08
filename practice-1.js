1feb9f9b03456bffaccb1ff473dd8a91435982c9
sumit-jha94 <sumitshine14@gmail.com>
2023-06-08 07:20:24 +0530

    added pratice-set 1

---
+++ practice-1.js
@@ -0,0 +1,44 @@
+// Chapter 1
+// Question -1 - Create a variable of type string and add try to add a number to it
+
+let a = "sumit"
+let b = 6
+console.log(a+b);
+
+// Question - 2 Find data type of question 1
+console.log(typeof (a+b))
+
+// Question -3 Create a const object in js. Can you change it to hold number later
+
+// const c = {
+//     nameOfStudent: "sumit",
+//     section: 1,
+//     students: 30
+// }
+
+// // c = 56
+
+// // No we cannot do
+
+// // Question -4 try to add a new key to the const object
+
+// c['friend'] = "Ramesh"
+// c['nameOfStudent'] = "ram"
+
+// console.log(c)
+
+// Question -5 write a js to create a word meaning dictionary of 5 words
+
+const dict = {
+    appreciate: "Good",
+    moodSwing: "change your mood",
+    laptop: "to take your knowledge"
+
+}
+
+console.log(dict)
+console.log(dict.appreciate)
+console.log(dict['laptop'])
+
+// There are two types of syntax to access the values
+
