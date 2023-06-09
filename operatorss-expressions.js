745879451f36665e03cf64c5cb10608807976921
sumit-jha94 <sumitshine14@gmail.com>
2023-06-09 07:17:06 +0530

    added operators and expression

---
+++ operatorss-expressions.js
@@ -0,0 +1,30 @@
+let a = 44;
+
+let b = 5;
+
+console.log( "Addition a + b = ", a+b);
+
+console.log( "Substraction a - b = ", a-b);
+
+console.log( "Multiply a * b = ", a*b);
+
+console.log( "Divide a / b = ", a/b);
+
+console.log( "Modulus (This show reminders) a % b = ", a%b);
+
+console.log( "Exponential a ** b = ", a**b);
+
+// console.log( "Increment a ++ = ", a++);
+// ++a wil add increase the value first and then print a
+// a++ will print a and then increase the value.
+
+console.log( "Increment ++a = ", ++a);
+
+
+// By using increment, the value of a has been increased. Now it will be treated as 45.
+// a = 46
+
+console.log( "Addition after increment a + b = ", a+b);
+
+console.log( "Decrement a -- = ", a--);
+
