994f933d09ee5ef8c17627adf4f2f5700f2bb340
sumit-jha94 <sumitshine14@gmail.com>
2023-06-16 06:57:10 +0530

    Practice Set - 2

---
+++ Practice Set - 2.js
@@ -0,0 +1,58 @@
+// // Question - 1
+
+// let age = parseInt(prompt("Enter Your Birth Year"));
+
+// age = 2023-age;
+
+// console.log(age)
+
+// if (age<=10 && age<=20) {
+//     alert("Your Ages Lies Between The Range")
+// } else {
+//     alert("Your Age Is Outside The Limit")
+// }
+
+// // Question - 2
+
+// let age = parseInt(prompt("Enter Your Birth Year"));
+
+// switch (age) {
+//     case 12:
+//         console.log("Your Age Is 12")
+//         break;
+
+//     case 10:
+//     console.log("Your Age Is 10")
+//     break;
+
+//     case 13:
+//     console.log("Your Age Is 13")
+//     break;
+    
+
+//     default:
+//         console.log("Your Age Is Not Special")
+//         break;
+// }
+
+// // Question - 3
+
+// let number = prompt("Enter the Number")
+
+// if (number % 2 == 0 && number % 3 == 0) {
+//     console.log("Number Is Divisible")
+// } else {
+//     console.log("Number Is Not Divisible")
+// }
+
+// Question - 4
+
+let age = prompt("Enter The Age")
+
+console.log( "You Can", (age>18? "Drive": "Cannot Drive"))
+
+// Other part
+
+let n = age > 18 ? "You can drive" : "You Cannot drive"
+
+console.log(n)
