4862d4b4ecb8fe290655907f30d02bfcca9ac992
sumit-jha94 <sumitshine14@gmail.com>
2023-06-17 07:59:32 +0530

    completed FOR Loop

---
+++ For Loops.js
@@ -0,0 +1,32 @@
+// for (let i = 2; i <=10; i++) {
+//     console.log(i)
+    
+// }
+
+// Program To Add First n natural no.
+
+// let sum = 0
+// let n = parseInt( prompt("Enter The No.") )
+
+// for (let i = 0; i < n; i++) {
+//     sum += (i+1)
+    
+//     console.log("sum is" ,sum += (i+1), "sum is")
+//     console.log((i+1))
+    
+// }
+// console.log( "sum is" + sum)
+
+// FOR-In Loop
+
+let obj = {
+    harry : 60,
+    ram : 45,
+    shyam : 33,
+    shiv: 56,
+    abhi: 78
+}
+
+for (let a in obj){
+    console.log(a)
+}
