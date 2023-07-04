9cf00f3104ff17f63dfd6cc2a92b291dc867e125
sumit-jha94 <sumitshine14@gmail.com>
2023-07-04 06:43:47 +0530

    Functions Finished

---
+++ Functions.js
@@ -0,0 +1,25 @@
+function Avg(x, y) {
+    console.log("Done")
+    return 1 + (x+y) / 2
+}
+
+// Arrow Function
+const hello = () => {
+    console.log("How Are You")
+    return "hi"
+}
+
+
+
+let a = 1;
+let b = 3;
+let c = 5;
+
+let v = hello();
+console.log(v);
+console.log("One Plus average a & b is", Avg(a, b))
+console.log("One Plus average b & c is", Avg(b, c), hello())
+hello();
+
+
+
