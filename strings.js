555326e9d7a1c9b03d819b0baada10133ab7afa1
sumit-jha94 <sumitshine14@gmail.com>
2023-07-16 00:06:41 +0530

    Finished Strings

---
+++ strings.js
@@ -0,0 +1,36 @@
+let myFriend = "Sumit jha";
+
+// console.log(myFriend.length)
+
+console.log(myFriend[0])
+console.log(myFriend[1])
+console.log(myFriend[2])
+console.log(myFriend[3])
+console.log(myFriend[4])
+console.log(myFriend[5])
+
+// // Single Quote
+
+// let tell = 'friend'
+
+// console.log(tell.length)
+
+// Template Liberals
+
+let boy1 = "Namit"
+let boy2 = "Nikhil"
+
+let sentence = `${boy1} is friend of ${boy2}`
+
+console.log(sentence)
+
+// escape sequence Character
+
+let fruit = 'Bana\'na'
+
+console.log(fruit)
+
+
+let watch = `Time\ns Error`
+
+console.log(watch)
