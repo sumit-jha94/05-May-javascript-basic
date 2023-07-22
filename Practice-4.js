a7f8de368f1cc07882f1e7089bc7a5e19de05aaf
sumit-jha94 <sumitshine14@gmail.com>
2023-07-22 17:42:12 +0530

    Added Practice 4

---
+++ Practice-4.js
@@ -0,0 +1,37 @@
+// // Question - 1
+
+// let str = "Har\""
+// console.log(str.length)
+
+// Question - 2
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const word = 'fox';
+
+console.log(sentence.includes("quick"))
+
+// console.log(`The word "${word}" ${sentence.includes(word) ? 'is' : 'is not'} in the sentence`);
+// Expected output: "The word "fox" is in the sentence
+
+// Question - 3
+
+const letter = 'This is JUST A CASE'
+
+console.log(letter.toLowerCase())
+
+console.log(letter.toUpperCase())
+
+// Question - 4
+
+let str2 = "Please give me Rs 1000"
+let amount = str2.slice("Please give me Rs ".length)
+console.log(amount)
+
+// Question - 5
+
+let frnd = "Were you able to do it"
+
+let frnd2 = frnd.replace(/able/i, "Ask")
+
+console.log(frnd2)
