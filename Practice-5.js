f41071fa1641be060ff9a1e0cfaf309b74ce9444
sumit-jha94 <sumitshine14@gmail.com>
2023-08-08 07:58:00 +0530

    Practice set on arrays done

---
+++ Practice-5.js
@@ -0,0 +1,52 @@
+// Question - 1
+
+// let arr = [1, 2, 3, 6, 9]
+
+// let a = prompt("Enter the Number")
+// a = Number.parseInt(a)
+// // c= arr.push(a)
+// // console.log(c)
+// arr.push(a)
+
+// console.log(arr)
+
+// Question - 2
+
+// let arr = [1, 2, 3, 6, 9]
+// let a;
+// do {
+//     a = prompt("Enter the Number")
+//     a = Number.parseInt(a)
+//     arr.push(a)
+// } while(a != 0);
+
+// console.log(arr)
+
+// // Question - 3
+
+// let arr = [1, 2, 3, 6, 9, 10, 12, 20, 880]
+
+// let n = arr.filter((x)=>{
+//     return x%10 == 0
+// })
+//  console.log(n)
+
+// //  Question - 4
+
+//  let arr = [1, 2, 3, 6, 9, 10, 12, 20, 880]
+
+//  let n = arr.map((x)=>{
+//         return x*x
+//     })
+//      console.log(n)
+
+//  Question - 5
+let arr = [1, 2, 3, 4]
+
+let n = arr.reduce((x1, x2)=>{
+            return x1*x2
+    })
+         console.log(n)
+
+
+
