64b47e0884f74e241649df00b025412bad8e1cc4
sumit-jha94 <sumitshine14@gmail.com>
2024-04-25 01:01:51 +0530

    Lect - 38 (Practice)

---
+++ Lect - 38 (practice qus)/app.js
@@ -0,0 +1,23 @@
+let firstElement = document.getElementsByClassName("firstElement")
+console.log(firstElement)
+firstElement[0].style.color="red"
+
+
+
+let secondElement = document.getElementsByClassName("secondElement")[0]
+console.log(secondElement)
+secondElement.style.color="red"
+
+
+
+
+// firstElement.style.background = "red",
+
+
+// cardTitle.style.color = "red"
+
+
+
+// let firstElement = document.getElementById("Home")
+// firstElement.style.color= "red"
+// console.log(firstElement)
