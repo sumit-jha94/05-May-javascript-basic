ddb08ddfda400c79d3d48ff1fde16a2c872cc54c
sumit-jha94 <sumitshine14@gmail.com>
2024-04-24 22:50:30 +0530

    Let - 36 (Searching The Dom)

---
+++ Lect - 36 (Searching DOM)/app.js
@@ -0,0 +1,15 @@
+// // 1 - getElementsByClassName
+// let cardTitle = document.getElementsByClassName("card-title")[0]
+
+// cardTitle.style.color = "red"
+
+// console.log(cardTitle)
+
+
+// 2 - querySelectorAll
+
+let cTitle = document.querySelectorAll(".card-title")
+
+cTitle[0].style.color = "blue"
+cTitle[1].style.color = "red"
+cTitle[2].style.color = "green"
