5ffb631ff015020918f6dec1101a824010f9d838
sumit-jha94 <sumitshine14@gmail.com>
2024-04-25 23:07:24 +0530

    Question Resolved

---
+++ Lect - 40 (Exercise - 3)/app.js
@@ -0,0 +1,339 @@
+let jokes = [
+    {
+        // "error": false,
+        // "amount": 10,
+        // "jokes": [
+            
+                "category": "Programming",
+                "type": "single",
+                "joke": "\"Knock, knock.\"\n\"Who's there?\"\n\n[very long pause]\n\n\"Java.\"",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 36,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "Java and C were telling jokes. It was C's turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" But Java didn't.",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 4,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "\"We messed up the keming again guys.\"",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 20,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "Two SQL tables sit at the bar. A query approaches and asks \"Can I join you?\"",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 221,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "Being a self-taught developer is almost the same as being a cut neck chicken because you have no sense of direction in the beginning.",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 184,
+                "safe": false,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 35,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "Saying that Java is nice because it works on every OS is like saying that anal sex is nice because it works on every gender.",
+                "flags": {
+                    "nsfw": true,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": true
+                },
+                "id": 30,
+                "safe": false,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "How do you tell HTML from HTML5?\n- Try it out in Internet Explorer\n- Did it work?\n- No?\n- It's HTML5.",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 43,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "Four engineers get into a car. The car won't start.\nThe Mechanical engineer says \"It's a broken starter\".\nThe Electrical engineer says \"Dead battery\".\nThe Chemical engineer says \"Impurities in the gasoline\".\nThe IT engineer says \"Hey guys, I have an idea: How about we all get out of the car and get back in\".",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": false
+                },
+                "id": 132,
+                "safe": true,
+                "lang": "en"
+            },
+            {
+                "category": "Programming",
+                "type": "single",
+                "joke": "Your mama's so FAT she can't save files bigger than 4GB.",
+                "flags": {
+                    "nsfw": false,
+                    "religious": false,
+                    "political": false,
+                    "racist": false,
+                    "sexist": false,
+                    "explicit": true
+                },
+                "id": 9,
+                "safe": false,
+                "lang": "en"
+            },
+
+
+            
+                // "error": false,
+                // "amount": 10,
+                // "jokes": [
+                    {
+                        "category": "Pun",
+                        "type": "single",
+                        "joke": "I'm reading a book about anti-gravity. It's impossible to put down!",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 126,
+                        "safe": true,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Misc",
+                        "type": "single",
+                        "joke": "In Soviet Russia, gay sex gets you arrested. In America, getting arrested gets you gay sex.",
+                        "flags": {
+                            "nsfw": true,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": true
+                        },
+                        "safe": false,
+                        "id": 289,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Programming",
+                        "type": "single",
+                        "joke": "There are only 10 kinds of people in this world: those who know binary and those who don't.",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 35,
+                        "safe": true,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Programming",
+                        "type": "single",
+                        "joke": "Java is like Alzheimer's, it starts off slow, but eventually, your memory is gone.",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 27,
+                        "safe": false,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Misc",
+                        "type": "single",
+                        "joke": "My husband and I were happy for 20 years. And then we met.",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "safe": true,
+                        "id": 273,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Programming",
+                        "type": "single",
+                        "joke": "\"We messed up the keming again guys.\"",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 20,
+                        "safe": true,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Programming",
+                        "type": "single",
+                        "joke": "A guy walks into a bar and asks for 1.4 root beers.\nThe bartender says \"I'll have to charge you extra, that's a root beer float\".\nThe guy says \"In that case, better make it a double.\"",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 2,
+                        "safe": true,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Programming",
+                        "type": "single",
+                        "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 24,
+                        "safe": true,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Programming",
+                        "type": "single",
+                        "joke": "I have a joke about Stack Overflow, but you would say it's a duplicate.",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "safe": true,
+                        "id": 301,
+                        "lang": "en"
+                    },
+                    {
+                        "category": "Dark",
+                        "type": "single",
+                        "joke": "My grandfather says I'm too reliant on technology.\nI called him a hypocrite and unplugged his life support.",
+                        "flags": {
+                            "nsfw": false,
+                            "religious": false,
+                            "political": false,
+                            "racist": false,
+                            "sexist": false,
+                            "explicit": false
+                        },
+                        "id": 178,
+                        "safe": false,
+                        "lang": "en"
+                    }
+
+                ]
+           
+console.log(jokes)
+
+let index = Math.floor(Math.random()* (jokes.length-1))
+console.log(index)
+joke.innerHTML = jokes[index].joke
