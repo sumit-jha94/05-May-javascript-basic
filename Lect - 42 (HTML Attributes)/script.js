7af94d312364db5e4159c3c62cc96f1729232013
sumit-jha94 <sumitshine14@gmail.com>
2024-04-27 17:46:51 +0530

    Lect-42(HTML Attributes)

---
+++ Lect - 42 (HTML Attributes)/script.js
@@ -0,0 +1,11 @@
+// getAttribute
+
+let a = first.getAttribute("class")
+console.log(a)
+
+let b = first.getAttribute("id")
+console.log(b)
+
+// setAttribute
+
+console.log(first.hasAttribute("class"))
