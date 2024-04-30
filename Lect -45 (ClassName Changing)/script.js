d076f70dde531b0103135ac615e9f0ffaedbbcd5
sumit-jha94 <sumitshine14@gmail.com>
2024-04-30 22:37:58 +0530

    Lect - 45 (Changing ClassName)

---
+++ Lect -45 (ClassName Changing)/script.js
@@ -0,0 +1,23 @@
+first.className = "text-dark yellow"
+
+// Removing a class
+
+first.classList.remove("yellow")
+
+// Adding a class
+
+first.classList.add("red")
+
+// Removing-Adding Again & Again (Toggle)
+
+let a = first.classList.toggle("red")
+
+console.log(a)
+
+
+
+
+
+
+
+
