d67868273e7d08fca43d2e36ca0be65c2fa659d3
sumit-jha94 <sumitshine14@gmail.com>
2024-05-01 22:07:47 +0530

    Lect - 46 (SetInterval+SetTimeout)

---
+++ Lect - 46 (SetInterval+SetTimeout)/script.js
@@ -0,0 +1,31 @@
+// This is used when to get the result after sometime
+
+
+// setTimeout(function() {
+//     alert("I am good")
+// }, 5000);
+
+// To stop using  timeout
+
+// let a = setTimeout(function() {
+//         alert("I am good")
+//     }, 5000);
+// console.log(a)
+// clearTimeout(a);
+
+// let c = setTimeout(function() {
+//     alert("I 2nd good")
+// }, 5000);
+
+// console.log(c)
+
+
+let a = setInterval(function() {
+    alert("Yes, I am running")
+}, 5000);
+
+console.log(a)
+
+// clearInterval
+
+// Cancel the execution of setInterval
