6b874495234e940a9c4128824ed7d8452d44aa8f
sumit-jha94 <sumitshine14@gmail.com>
2024-05-15 00:55:17 +0530

    Lect - 60 (Error Handling)

---
+++ Lect - 60 (Error Handling)/script.js
@@ -0,0 +1,21 @@
+// Try--catch
+
+
+try {
+    console.log(rahul)
+} catch (error) {
+    console.log("Try-Catch "+error)
+}
+
+setTimeout(() => {
+    console.log("Hacking Wifi after 1s")
+}, 1000);
+
+setTimeout(() => {
+    console.log("Hacking Wifi after 2s")
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hacking Wifi after 3s")
+}, 2000);
+
