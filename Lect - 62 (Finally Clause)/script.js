23c16ab0440ed3a82dd797df688f25a4653e7194
sumit-jha94 <sumitshine14@gmail.com>
2024-05-16 00:56:30 +0530

    Lect - 62 (Finally Clause)

---
+++ Lect - 62 (Finally Clause)/script.js
@@ -0,0 +1,15 @@
+// Finally runs in every case, with error or without error
+
+try {
+    let a = 1;
+    // console.log(z)
+    console.log("Success")
+    // it runs if we have return
+    // return
+} catch (error) {
+    console.log("This is error")
+}
+
+finally{
+    console.log("Finally Clause")
+}
